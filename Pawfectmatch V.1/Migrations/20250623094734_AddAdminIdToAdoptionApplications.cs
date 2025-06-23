988bc24a5d72195f740f6f52e91752440eb76b1b
Angel Aubrey V. Colinares <angelaubreyvcolinares@iskolarngbayan.pup.edu.ph>
2025-06-23 17:53:07 +0800

    update AdminId column in database

---
+++ Pawfectmatch V.1/Migrations/20250623094734_AddAdminIdToAdoptionApplications.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pawfectmatch_V._1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminIdToAdoptionApplications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdminId",
+                table: "AdoptionApplications",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdminId",
+                table: "AdoptionApplications",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
